48d4404767660c718666b8d9723498747676014c
yican <wangyidong@MacBook-Pro-2.local>
2021-08-17 17:48:56 +0800

    fix: location问题

--- pocket/react-vite-h5/src/components/NavBar/index.jsx
+++ pocket/react-vite-h5/src/components/NavBar/index.jsx
@@ -2,24 +2,24 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 import { TabBar } from 'zarm';
 import { useHistory } from 'react-router-dom';
-import s from './style.module.less';
 import CustomIcon from '../CustomIcon'
+import s from './style.module.less';
 
 const NavBar = ({ showNav }) => {
   const [activeKey, setActiveKey] = useState('/');
   const history = useHistory()
 
-  const changeTab = (path) => {
+  const chnageTab = (path) => {
     setActiveKey(path)
     history.push(path)
   }
 
   return (
-    <TabBar visible={showNav} className={s.tab} activeKey={activeKey} onChange={changeTab}>
+    <TabBar visible={showNav} className={s.tab} activeKey={activeKey} onChange={chnageTab}>
       <TabBar.Item
         itemKey="/"
         title="账单"
-        icon={<CustomIcon type="zhangdan"/>}
+        icon={<CustomIcon type="zhangdan" />}
       />
       <TabBar.Item
         itemKey="/data"
--- pocket/react-vite-h5/src/router/index.js
+++ pocket/react-vite-h5/src/router/index.js
@@ -1,22 +1,7 @@
-// import Index from '../container/Index'
-// import About from '../container/About'
-
-// const routes = [
-//   {
-//     path: "/",
-//     component: Index
-//   },
-//   {
-//     path: "/about",
-//     component: About
-//   }
-// ];
-
-// export default routes
-
-import Home from '../container/Home'
-import Data from '../container/Data'
-import User from '../container/User'
+import Home from '@/container/Home'
+import Data from '@/container/Data'
+import User from '@/container/User'
+import Detail from '@/container/Detail'
 
 const routes = [
   {
@@ -30,6 +15,10 @@ const routes = [
   {
     path: "/user",
     component: User
+  },
+  {
+    path: "/detail",
+    component: Detail
   }
 ];
 
