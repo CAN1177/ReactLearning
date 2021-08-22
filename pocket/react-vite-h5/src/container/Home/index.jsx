025c1e7cda6be10de99efc4ac98d9d61a79e7623
CAN7 <39960526+CAN1177@users.noreply.github.com>
2021-08-22 21:44:58 +0800

    feat: 账单添加弹窗

--- pocket/react-vite-h5/src/container/Home/index.jsx
+++ pocket/react-vite-h5/src/container/Home/index.jsx
@@ -1,12 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Icon, Pull } from 'zarm'
 import dayjs from 'dayjs'
 import BillItem from '@/components/BillItem'
-import { get, REFRESH_STATE, LOAD_STATE } from '@/utils' // Pull 组件需要的一些常量
+import PopupType from '@/components/PopupType'
+import PopupDate from '@/components/PopupDate'
+import { get, REFRESH_STATE, LOAD_STATE } from '@/utils'
+
+import CustomIcon from '@/components/CustomIcon'
+import PopupAddBill from '@/components/PopupAddBill'
 
 import s from './style.module.less'
 
 const Home = () => {
+  const addRef = useRef()
+  const typeRef = useRef(); // 账单类型 ref
+  const monthRef = useRef(); // 月份筛选 ref
+  const [totalExpense, setTotalExpense] = useState(0); // 总支出
+  const [totalIncome, setTotalIncome] = useState(0); // 总收入
+  const [currentSelect, setCurrentSelect] = useState({}); // 当前筛选类型
   const [currentTime, setCurrentTime] = useState(dayjs().format('YYYY-MM')); // 当前筛选时间
   const [page, setPage] = useState(1); // 分页
   const [list, setList] = useState([]); // 账单列表
@@ -16,17 +27,19 @@ const Home = () => {
 
   useEffect(() => {
     getBillList() // 初始化
-  }, [page])
+    console.log('typeRef', typeRef)
+  }, [page, currentSelect, currentTime])
 
-  // 获取账单方法
   const getBillList = async () => {
-    const { data } = await get(`/api/bill/list?page=${page}&page_size=5&date=${currentTime}`);
+    const { data } = await get(`/api/bill/list?page=${page}&page_size=5&date=${currentTime}&type_id=${currentSelect.id || 'all'}`);
     // 下拉刷新，重制数据
     if (page == 1) {
       setList(data.list);
     } else {
       setList(list.concat(data.list));
     }
+    setTotalExpense(data.totalExpense.toFixed(2));
+    setTotalIncome(data.totalIncome.toFixed(2));
     setTotalPage(data.totalPage);
     // 上滑加载状态
     setLoading(LOAD_STATE.success);
@@ -50,18 +63,46 @@ const Home = () => {
     }
   }
 
+  // 添加账单弹窗
+  const toggle = () => {
+    typeRef.current && typeRef.current.show()
+  };
+  // 选择月份弹窗
+  const monthToggle = () => {
+    monthRef.current && monthRef.current.show()
+  };
+
+  // 筛选类型
+  const select = (item) => {
+    setRefreshing(REFRESH_STATE.loading);
+    setPage(1);
+    setCurrentSelect(item)
+  }
+  // 筛选月份
+  const selectMonth = (item) => {
+    setRefreshing(REFRESH_STATE.loading);
+    setPage(1);
+    setCurrentTime(item)
+  }
+
+  // 添加账单弹窗
+  const addToggle = () => {
+    addRef.current && addRef.current.show()
+  }
+
   return <div className={s.home}>
+    
     <div className={s.header}>
       <div className={s.dataWrap}>
-        <span className={s.expense}>总支出：<b>¥ 200</b></span>
-        <span className={s.income}>总收入：<b>¥ 500</b></span>
+        <span className={s.expense}>总支出：<b>¥ { totalExpense }</b></span>
+        <span className={s.income}>总收入：<b>¥ { totalIncome }</b></span>
       </div>
       <div className={s.typeWrap}>
-        <div className={s.left}>
-          <span className={s.title}>类型 <Icon className={s.arrow} type="arrow-bottom" /></span>
+        <div className={s.left} onClick={toggle}>
+          <span className={s.title}>{ currentSelect.name || '全部类型' } <Icon className={s.arrow} type="arrow-bottom" /></span>
         </div>
         <div className={s.right}>
-          <span className={s.time}>2022-06<Icon className={s.arrow} type="arrow-bottom" /></span>
+          <span className={s.time} onClick={monthToggle}>{ currentTime }<Icon className={s.arrow} type="arrow-bottom" /></span>
         </div>
       </div>
     </div>
@@ -89,6 +130,10 @@ const Home = () => {
         </Pull> : null
       }
     </div>
+    <div className={s.add} onClick={addToggle}><CustomIcon type='tianjia' /></div>
+    <PopupType ref={typeRef} onSelect={select} />
+    <PopupDate ref={monthRef} mode="month" onSelect={selectMonth} />
+    <PopupAddBill ref={addRef} />
   </div>
 }
 
